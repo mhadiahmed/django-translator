1a485bdab82df205df1d96ccbf3c768ebe499880
Mhadi Ahmed <mhadiahmed63@gmail.com>
2017-12-24 09:20:04 +0300

    static root heroku

--- translator/settings/production.py
+++ translator/settings/production.py
@@ -123,7 +123,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
 STATIC_URL = '/static/'
-
+STATIC_ROOT = os.path.join((BASE_DIR),"static_cdn")
 CORS_REPLACE_HTTPS_REFERER      = True
 HOST_SCHEME                     = "https://"
 SECURE_PROXY_SSL_HEADER         = ('HTTP_X_FORWARDED_PROTO', 'https')
