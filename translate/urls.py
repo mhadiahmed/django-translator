7aba37673ac88e374eeea4cd0ab28a0d62e35f10
Mhadi Ahmed <mhadiahmed63@gmail.com>
2017-12-24 09:55:23 +0300

    static files 8

--- translate/urls.py
+++ translate/urls.py
@@ -4,3 +4,6 @@ from . import views
 urlpatterns = [
     url(r'^$', views.home),
 ]
+urlpatterns += patterns('',
+ (r'^static/(?P.*)$', 'django.views.static.serve', {'document_root': settings.STATIC_ROOT}),
+ )
