797b2f7aac146110340285210edbeaebb7a76b39
Mhadi Ahmed <mhadiahmed63@gmail.com>
2017-12-25 08:43:02 +0300

    save changeson wsgifile

--- translator/wsgi.py
+++ translator/wsgi.py
@@ -15,4 +15,4 @@ from whitenoise.django import DjangoWhiteNoise
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "translator.settings")
 
 application = get_wsgi_application()
-application = DjangoWhiteNoise(application)
+
